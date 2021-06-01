60a3bb92ec4d31597516de45baa33d332d5da6a3
3atmancoding <3atmancoding@gmail.com>
2021-06-01 19:10:07 +0530

    Find vowels in a string

--- Strings/vowelFilter/vowelFilter.js
+++ Strings/vowelFilter/vowelFilter.js
@@ -1,9 +1,26 @@
 function vowelInString(str) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   let arr = str.split('');
+  arr = arr.reduce((acc, curr) => {
+    return vowels.indexOf(curr) > -1 ? acc.concat(curr) : acc;
+  }, []);
+  return arr.join('');
+}
+const str = 'america';
+vowelInString(str);
+
+//Output :: "aeia"
+
+// Solution to find in form of array
+
+function vowelInString(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let arr = str.split('');
   return arr.reduce((acc, curr) => {
     return vowels.indexOf(curr) > -1 ? acc.concat(curr) : acc;
   }, []);
 }
 const str = 'america';
 vowelInString(str);
+
+//Output ::  ["a", "e", "i", "a"]
