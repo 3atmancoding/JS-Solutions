d20243277f8874ae29bcdefd579b7d590b9e2986
3atmancoding <62512325+3atmancoding@users.noreply.github.com>
2021-06-01 18:39:18 +0530

    Update filter.js

--- Strings/Duplicates/filter.js
+++ Strings/Duplicates/filter.js
@@ -5,6 +5,11 @@
 /** Using filter method
  * @param str string
  */
+
+/* Solution explained: So each element's index is matched with current index. Since the indexOf finds first index of any matching element here 'l' will first occur
+on index 2 when i = 2, i++ and i =3 but indexOf still finds first l on index 2. Hence we find the duplicate
+*/
+
 let str = 'Hello';
 function duplicateInString(str) {
   let newStr = [];
