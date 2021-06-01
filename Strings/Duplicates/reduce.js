7f565fe51dfc599e2ee6e0513284316c352e17b9
3atmancoding <62512325+3atmancoding@users.noreply.github.com>
2021-06-01 18:49:52 +0530

    Update reduce.js

--- Strings/Duplicates/reduce.js
+++ Strings/Duplicates/reduce.js
@@ -4,6 +4,14 @@
  */
 /** Using reduce method
  * @param str string
+ 
+ Solution Explained
+ ------------------
+ 1. We split the string to convert into an array
+ 2. Reduce function -> accmuluator is a blank array intially, current value is the first index value that is 'H'
+ 3. We check the condition if array has any element which is missing since indexOf is less than 0 we push in current value inside the array
+ 4. When we check for second 'l', array already has the value which makes indexOf > 0 hence we omit the second 'l' which is a duplicate
+ 
  */
 let str = 'hello';
 function remove(str) {
